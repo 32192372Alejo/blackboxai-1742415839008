ca87471aa9825736c637b1bbb2b889d3944a609c
32192372Alejo <alejo290816@gmail.com>
2025-03-19 15:25:54 -0500

    Add FirstLayoutActivity.kt 1742415841668

---
+++ FirstLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirstLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_layout)
+    }
+}
